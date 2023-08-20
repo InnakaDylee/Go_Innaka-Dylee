94dbf86dfb208dea73319afbdff3e778e9b3374b
Innaka <innakadyleexd@gmail.com>
2023-08-21 06:11:06 +0700

    Update Feaature

--- 03_Version_Control_and_Branch_Management(Git)/Praktikum/main.go
+++ 03_Version_Control_and_Branch_Management(Git)/Praktikum/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main(){
 	fmt.Println("Hello World!!")
 	fmt.Println("Add Develop")
-	fmt.Println("Add Featured A")
-	fmt.Println("Add Featured F")
+	fmt.Println("Edit Featured A")
+	fmt.Println("Remove Feature F")
 	fmt.Println("Add Feature B")
 	fmt.Println("Add Feature C")
 	fmt.Println("Add Feature D")
